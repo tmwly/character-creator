33147cc49f4a5a622a51ad0dc9567239961e2fd9
Tom Wiley <tomos.g.wiley@gmail.com>
2021-05-26 10:51:30 +0100

    completed exercise

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import ShadedBackground from './components/ShadedBackground';
 
 function App() {
   return (
     <>
+      <ShadedBackground />
       <CharacterEditor />
       <Footer />
     </>
---
+++ src/components/ShadedBackground/ShadedBackground.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './ShadedBackground.module.css';
+
+const ShadedBackground = () => {
+  return (
+    <div className={styles.ShadedBackground} />
+  );
+};
+
+export default ShadedBackground;
